559c76ccb7108966383267f38aec7f83816ef8d0
Francisco Sousa <francisco.sousa@hey.com>
2022-08-29 17:07:42 +0100

    Refresh on 5 mins idle

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,20 @@
 <script>
+	import { onMount } from 'svelte';
+	import IdleTracker from 'idle-tracker';
+
 	import '../reset.css';
+
+	onMount(() => {
+		const idleTracker = new IdleTracker({
+			// 5 minutes
+			timeout: 300000,
+			onIdleCallback: () => {
+				location.reload();
+			}
+		});
+
+		idleTracker.start();
+	});
 </script>
 
 <slot />
